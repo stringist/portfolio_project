fc2e6aeb147900709fa7edd85eec360144285a39
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-03-20 19:42:38 +0100

    responsive animation done

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 "use strict";
+gsap.registerPlugin(ScrollTrigger);
 
 window.addEventListener("load", start)
 
-const tlArrowBounce = new TimelineMax({ repeat: -1 });
 
 function start() {
     console.log("ready");
 }
+
+
+// animations
+
 const downArrow = document.querySelector(".arrow_container");
+const tlArrowBounce = new TimelineMax({ repeat: -1 });
 
 tlArrowBounce.
 to(downArrow, { y: '-8', ease: Sine.easeInOut, duration: .8 }).
 to(downArrow, { y: '+=8', ease: Sine.easeInOut, duration: .8 });
+
+
+const screen = document.querySelector(".device_container");
+const screentl = gsap.timeline({ repeat: -1, delay: .5, repeatDelay: .5 });
+
+screentl.to(screen, { duration: 1, scaleX: .5, scaleY: .5, rotation: 90, ease: "sine" });
+screentl.to(screen, { duration: 1, scaleX: .5, scaleY: .6, delay: 1, ease: "bounce" });
+screentl.to(screen, { duration: 1, rotation: 90, delay: 0, ease: "sine" });
+screentl.to(screen, { duration: 1, rotation: 180, delay: 0, ease: "sine" });
+screentl.to(screen, { duration: 1, scaleX: 1, scaleY: 1, delay: 1, ease: "circ" });
